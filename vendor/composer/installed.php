cf9b2296ba6f4379568dba405bd58cb5a814d0eb
Sérgio Santos <me@s3rgiosan.com>
2025-06-09 15:20:33 +0100

    Add vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '10up/elasticpress-content-connect',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ca5d35e6154259bac7b35321b846a39b3c57a46e',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '10up/elasticpress-content-connect' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ca5d35e6154259bac7b35321b846a39b3c57a46e',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
